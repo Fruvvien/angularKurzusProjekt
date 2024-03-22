27cf3103c4da08c7dd68367c3f8627d5c8f68823
Gabi <HunFantasztik@gmail.com>
2024-03-22 14:29:58 +0100

    saddsa

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'reactive-Forms';
+export class AppComponent implements OnInit {
+  
+  submitDatas: FormGroup;
+
+
+  ngOnInit() {
+      this.submitDatas = new FormGroup({
+        'projectname' : new FormControl(null,[Validators.required, this.forbiddenProjectName], this.asyncInvalidProjectName),
+        'email' : new FormControl(null,[Validators.required, Validators.email]),
+        'projectStatus' : new FormControl('critical')
+      });
+  }
+
+  forbiddenProjectName(control: FormControl): {[s: string]: boolean}{
+    if(control.value === 'Test'){
+      return{"projectForbidden": true};
+    }
+    return null;
+  }
+  onSubmit(){
+    console.log(this.submitDatas.value)
+  }
+  asyncInvalidProjectName(control: FormControl) : Promise<any> | Observable <any>{
+    const promise = new Promise((resolve, reject) =>{
+      setTimeout(() => {
+        if(control.value === 'Testproject'){
+          resolve({"projectForbidden": true});
+
+        }
+        else{
+          resolve(null);
+        }
+      }, 2000);
+    })
+    return promise;
+  }
+
 }
